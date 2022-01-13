9530660dbc3069faa9f63d29f68a183a43dd0935
BRazvann <razvanbornac_98@yahoo.com>
2022-01-14 01:29:04 +0200

    modified homepage and started working on dashboard
    rebasing with danut branch

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import "./App.css";
-import { makeStyles } from '@material-ui/core/styles';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-
   const useStyles = makeStyles((theme) => ({
     root: {
-        fontFamily: 'Garamond',
-        color: '#63be47'
+      fontFamily: "Garamond",
+      color: "#63be47",
     },
   }));
 
   const classes = useStyles();
 
   const errorMessage = () => {
-    return <h1 className={classes.root}>IT LOOKS LIKE THIS PAGE DOESN'T EXIST!</h1>;
-  }
+    return (
+      <h1 className={classes.root}>IT LOOKS LIKE THIS PAGE DOESN'T EXIST!</h1>
+    );
+  };
 
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
-          <Route path='/*'>
-            {errorMessage}
-          </Route>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="*">{errorMessage}</Route>
         </Switch>
       </div>
     </Router>
