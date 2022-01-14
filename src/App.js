9b6939d859a7e5d52644142c7fba9ed12a655f60
BRazvann <razvanbornac_98@yahoo.com>
2022-01-14 14:17:53 +0200

    fixed the error message for invalid pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import "./App.css";
-import { makeStyles } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ErrorMessage from "./pages/ErrorMessage";
 
 function App() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      fontFamily: "Garamond",
-      color: "#63be47",
-    },
-  }));
-
-  const classes = useStyles();
-
-  const errorMessage = () => {
-    return (
-      <h1 className={classes.root}>IT LOOKS LIKE THIS PAGE DOESN'T EXIST!</h1>
-    );
-  };
-
   return (
     <Router>
       <div className="App">
@@ -30,7 +15,7 @@ function App() {
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
           <Route path="/dashboard" component={Dashboard} />
-          <Route path="*">{errorMessage}</Route>
+          <Route path="*" component={ErrorMessage} />
         </Switch>
       </div>
     </Router>
